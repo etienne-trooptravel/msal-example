8669a34b077580a79f5eddd5e580013e6ca82d8b
etienne-trooptravel <etienne@trooptravel.com>
2020-05-15 08:05:53 +0200

    Added basic sign in

--- src/main.js
+++ src/main.js
@@ -2,11 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import msal from 'vue-msal'
 
 Vue.config.productionTip = false
+
+Vue.use(msal, {
+    auth: {
+      clientId: '3f9b0475-0293-462a-9273-48eafbdc2d44',
+      // redirectUri: '',
+      requireAuthOnInitialize: true
+    }
+});
 
 new Vue({
   router,
   store,
+  created() {
+    if (!this.$msal.isAuthenticated()) {
+        this.$msal.signIn();
+    }
+  },
   render: h => h(App)
 }).$mount('#app')
