a3004cac9688c52e3570205b7f7b762f6b031cb7
etienne-trooptravel <etienne@trooptravel.com>
2020-05-18 01:39:15 +0200

    Print userdata to view if user loaded

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,51 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Sample Functions</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
+      <div v-if="user">
+        <div>Welcome {{ user.name }}</div>
+        <div v-if="user.profile.jobTitle">Your job title is {{ user.profile.jobTitle }}</div>
+        <div><button @click="$msal.signOut()">Log out</button></div>
+    </div>
+    <div v-else>
+      <button @click="$msal.signIn()">Log in</button>
+    </div>
+      <!-- <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li> -->
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
   </div>
 </template>
 
 <script>
+import store from '../store'
+import { msalMixin } from 'vue-msal';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  mixins: [msalMixin],
+  computed: {
+    user() {
+      let user = null;
+      if (this.msal.isAuthenticated) {
+        user = {
+          ...this.msal.user,
+          profile: {}
+        }
+        if (this.msal.graph && this.msal.graph.profile) {
+          user.profile = this.msal.graph.profile
+          console.log(`USER Profile: `, user.profile);
+        }
+        store.commit('setUser', user);
+      }
+      return user;
+    }
   }
+  // computed: {
+  //   ...mapState(['user'])
+  // }
 }
 </script>
 
