76a4a715a96cf9d712506896d518aeae2dc10e2c
etienne-trooptravel <etienne@trooptravel.com>
2020-05-18 01:40:35 +0200

    Basic setup with MSAL Auth and userdata from MS Graph

--- src/main.js
+++ src/main.js
@@ -9,24 +9,43 @@ Vue.config.productionTip = false
 Vue.use(msal, {
     auth: {
       clientId: '3f9b0475-0293-462a-9273-48eafbdc2d44',
+      tenantId: '62065239-3278-4fc0-9754-fb53d27cddb6',
       redirectUri: 'https://vue-msal-example.firebaseapp.com/',
-      // https://vue-msal-example.firebaseapp.com/.auth/login/aad/callback
-      requireAuthOnInitialize: true,
+      // redirectUri: 'http://localhost:8080/',
+      // requireAuthOnInitialize: true,
       onAuthentication(ctx, error, response) {
-        console.log(ctx),
-        console.log(error),
-        console.log(response)
+        console.log(`CTX: `, ctx),
+        console.log(`ERROR: `, error),
+        console.log(`RESPONSE: `, response)
       }
+    },
+    request: {
+      scopes: [
+        "user.read",
+        "user.read.all",
+        "profile"
+      ]
+    },
+    graph: {
+      callAfterInit: true,
+      onResponse(ctx, response) {
+        console.log(`GRAPH CTX: `, ctx),
+        console.log(`GRAPH RESPONSE: `, response)
+      }
+    },
+    cache: {
+      // cacheLocation: "sessionStorage" - more secure but lacs SSO accross tabs
+      cacheLocation: "localStorage"
     }
+    // framework: {
+    //   globalMixin: true
+    // }
 });
+
+// api://vue-msal-example.firebaseapp.com/3f9b0475-0293-462a-9273-48eafbdc2d44
 
 new Vue({
   router,
   store,
-  created() {
-    if (!this.$msal.isAuthenticated()) {
-        this.$msal.signIn();
-    }
-  },
   render: h => h(App)
 }).$mount('#app')
